0811239429f759e8eeb783da01152ab860607da1
pidokige02 <pidokige0204@gmail.com>
2022-08-29 22:24:28 +0900

    #1.4 Apollo Dev Tools

--- src/routes/Movie.js
+++ src/routes/Movie.js
@@ -1,3 +1,27 @@
+import { gql, useQuery } from "@apollo/client";
+import { useParams } from "react-router-dom";
+
+// graphql 에서 사용한 query field 와 동잏한 syntax 를 사용함
+const GET_MOVIE = gql`
+  query getMovie($movieId: String!) {
+    movie(id: $movieId) {
+      id
+      title
+      small_cover_image
+    }
+  }
+`;
+
 export default function Movie() {
-  return <div>This is movie details</div>;
+  const { id } = useParams(); // id parameter from URI
+  const { data, loading } = useQuery(GET_MOVIE, {
+    variables: {  // the same format as we did in graphql server in variable section
+      movieId: id,
+    },
+  });
+  console.log(data, loading);
+  if (loading) {
+    return <h1>Fetching movie...</h1>;
+  }
+  return <div>{data.movie.title}</div>;
 }
--- src/routes/Movies.js
+++ src/routes/Movies.js
@@ -1,4 +1,5 @@
 import { gql, useQuery } from "@apollo/client";
+import { Link } from "react-router-dom";
 
 const ALL_MOVIES = gql`
   query getMovies {
@@ -17,7 +18,6 @@ const ALL_MOVIES = gql`
 `;
 
 export default function Movies() {
-  // 선언형 code 는 원하는 것을 설명하기 위한 code만 적는 가을 말한다.
   const { data, loading, error } = useQuery(ALL_MOVIES);
   if (loading) {
     return <h1>Loading...</h1>;
@@ -29,12 +29,8 @@ export default function Movies() {
     <ul>
       <h1>Movies</h1>
       {data.allMovies.map((movie) => (
-        <li key={movie.id}>{movie.title}</li>
-      ))}
-      <h1>Tweets</h1>
-      {data.allTweets.map((tweet) => (
-        <li key={tweet.id}>
-          {tweet.text}/by: {tweet.author.fullName}
+        <li key={movie.id}>
+          <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
         </li>
       ))}
     </ul>
