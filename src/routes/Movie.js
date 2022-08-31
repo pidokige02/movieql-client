24bfce4e7f4e4e02374f0cd85456cdfb8bcc2263
pidokige02 <pidokige0204@gmail.com>
2022-08-31 21:56:17 +0900

    #2.1 Local Only Fields

--- src/routes/Movie.js
+++ src/routes/Movie.js
@@ -1,5 +1,6 @@
 import { gql, useQuery } from "@apollo/client";
 import { useParams } from "react-router-dom";
+import styled from "styled-components";
 
 // graphql 에서 사용한 query field 와 동잏한 syntax 를 사용함
 const GET_MOVIE = gql`
@@ -7,9 +8,47 @@ const GET_MOVIE = gql`
     movie(id: $movieId) {
       id
       title
-      small_cover_image
+      medium_cover_image
+      rating
+      isLiked @client
     }
   }
+`;
+
+const Container = styled.div`
+  height: 100vh;
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: white;
+`;
+
+const Column = styled.div`
+  margin-left: 10px;
+  width: 50%;
+`;
+
+const Title = styled.h1`
+  font-size: 65px;
+  margin-bottom: 15px;
+`;
+
+const Subtitle = styled.h4`
+  font-size: 35px;
+  margin-bottom: 10px;
+`;
+
+
+const Image = styled.div`
+  width: 25%;
+  height: 60%;
+  background-color: transparent;
+  background-image: url(${(props) => props.bg});
+  background-size: cover;
+  background-position: center center;
+  border-radius: 7px;
 `;
 
 export default function Movie() {
@@ -20,8 +59,14 @@ export default function Movie() {
     },
   });
   console.log(data, loading);
-  if (loading) {
-    return <h1>Fetching movie...</h1>;
-  }
-  return <div>{data.movie.title}</div>;
+  return (
+    <Container>
+      <Column>
+        <Title>{loading ? "Loading..." : `${data.movie?.title}`}</Title>
+        <Subtitle>⭐️ {data?.movie?.rating}</Subtitle>
+        <button>{data?.movie?.isLiked ? "Unlike" : "Like"}</button>
+      </Column>
+      <Image bg={data?.movie?.medium_cover_image} />
+    </Container>
+  );
 }
